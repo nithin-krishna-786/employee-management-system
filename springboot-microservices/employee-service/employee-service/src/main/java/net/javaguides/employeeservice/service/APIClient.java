bb2101754ecf898d3c037070bec99f885cd0ebcb
Nithin <nknithinabc@gmail.com>
2023-12-28 18:39:03 +0530

    COMMIT2

--- springboot-microservices/employee-service/employee-service/src/main/java/net/javaguides/employeeservice/service/APIClient.java
+++ springboot-microservices/employee-service/employee-service/src/main/java/net/javaguides/employeeservice/service/APIClient.java
@@ -8,7 +8,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(url = "http://localhost:8080/api/departments/",value = "DEPARTMENT-SERVICE")
+//@FeignClient(url = "http://localhost:8080/api/departments/",value = "DEPARTMENT-SERVICE") //ORIG WITH 1 INSTANCE
+@FeignClient(name = "DEPARTMENT-SERVICE") 
 public interface APIClient {
     // Build get department rest api
     @GetMapping("api/departments/{department-code}")
